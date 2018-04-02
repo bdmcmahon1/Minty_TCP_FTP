5c2bdf66d364d7e8d7cc0340c9479c8426e4feb3
Charlie Sullivan <csullivan107@gmail.com>
2018-04-02 13:03:53 -0600

    syntax bugs fixed

--- CharlieTCPclientV1.0.py
+++ CharlieTCPclientV1.0.py
@@ -4,19 +4,19 @@
 
 import socket
 
-sAddr = ('localhost',50000)
+saddr = ('localhost',10000)
 
 buf_size = 1024
 
 
 print "etablishing client"
-client=socket.socket(socket.AT_INET,socket.SOCK_STREAM)
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print "connecting to server"
 client.connect(saddr)
 print "sending Data(hello world)"
 client.send('hello world')
 print "waiting for response"
-response = s.recv(buf_size)
+response = client.recv(buf_size)
 
 s.close()
-print "response: " response
+print "response: " ,response
